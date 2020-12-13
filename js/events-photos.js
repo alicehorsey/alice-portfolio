cd0bccd710ab21ef325681e7647ae01d10955758
Alice Horsey <alicehorsey@gmail.com>
2020-12-13 17:00:31 +0000

    home page css and layout working inc links and nav

--- js/events-photos.js
+++ js/events-photos.js
@@ -1,17 +1,32 @@
 const backgroundColors = {
-    1: "#f4faf3",
-    2: "#fee1eb",
-    3: "#fae1d6",
-    4: "#e8e8f0"
+    1: ["black", "white"],
+    2: ["purple", "black"],
+    3: ["green", "purple"],
+    4: ["yellow", "blue"],
+    5: ["blue", "white"],
+    6: ["orange", "green"],
+    7: ["violet", "black"],
+    8: ["pink", "black"],
+    9: ["grey", "yellow"],
+    10: ["turquoise", "blue"],
+
 }
 
 function randomNum() {
-    return Math.floor(Math.random() * 4 + 1)
+    return Math.floor(Math.random() * 10 + 1)
 }
 
-function changeColour() {
-    let photos = document.getElementsByClassName("flip-card-back");
-    for (let i = 0; i < photos.length; i++) {
-        photos[i].style.backgroundColor = backgroundColors[randomNum()]
+function setInitialColour() {
+    let cards = document.getElementsByClassName("flip-card-back");
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].style.backgroundColor = backgroundColors[randomNum()][0]
+        cards[i].style.color = backgroundColors[randomNum()][1]
     }
+}
+
+function changeColour(card) {
+    setTimeout(() => {
+        card.style.backgroundColor = backgroundColors[randomNum()][0]
+        card.style.color = backgroundColors[randomNum()][1]
+    }, 500)
 }
